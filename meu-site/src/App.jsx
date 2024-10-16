d26248db5b77e4c2149475edea8e91254ecc2550
Luis Eduardo <119073361+luiseduardo010@users.noreply.github.com>
2024-10-16 19:24:56 +0000

    meu site

---
+++ meu-site/src/App.jsx
@@ -0,0 +1,26 @@
+export default function App() {
+  return (
+    <div className="App">
+      <img src="react.png"/>
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuários web nativas.</p>
+      <button>Aprenda React</button> <button>Referência da API</button>
+      <hr />
+      <h2>Crie interfaces de usuários de componentes</h2>
+      <p>React permite que você construa interaces de usuário a partir de pedaços individuais chamados componentes.</p>
+      <hr/>
+      <h2>Escreva componentes com código e marcação</h2>
+      <p>Componentes React são funções JavaScript. A sintaxe e marcação é chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo React.</p>
+      <hr/>
+      <h2>Próximos passos:</h2>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos o JavaScript</li>
+        
+      </ul>
+    </div>
+  )
+}
